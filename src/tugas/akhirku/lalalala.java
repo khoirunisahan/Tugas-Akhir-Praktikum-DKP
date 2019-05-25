6d2d0c233ea73d23db3fe567c89a94b76ea2450d
khoirunisahan <hanyhandayani011@gmail.com>
2019-05-25 07:13:47 +0700

    first commit

---
+++ src/tugas/akhirku/lalalala.java
@@ -0,0 +1,25 @@
+package tugas.akhirku;
+
+public class lalalala {
+    private String berlaku,jenis;
+    private int harga;
+
+    public void setBerlaku(String berlaku){
+        this.berlaku=berlaku;
+}
+    public void setjenis(String jenis){
+        this.jenis=jenis;
+}
+    public void setharga(int harga){
+        this.harga=harga;
+}
+    public int getharga(){
+        return harga;
+    }
+    public String getjenis(){
+        return jenis;
+    }
+    public String getberlaku(){
+        return berlaku;
+    }
+}
